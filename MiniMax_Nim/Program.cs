ee1ab036187c10338b4436b85ee5087139a226e2
rybrrr <kopejskat@gchd.cz>
2025-11-03 09:35:13 +0100

    Fix loop bounds in minimax algorithm
    Updated the loop condition in the minimax function to iterate over piles.Count * 2 instead of piles.Count * 2 - 1, ensuring all possible moves are considered for both maximizing and minimizing players.

--- MiniMax_Nim/Program.cs
+++ MiniMax_Nim/Program.cs
@@ -121,7 +121,7 @@ namespace MiniMax_Nim_2
                 if (maximizingPlayer)
                 {
                     int best = -1;
-                    for (int i = 0; i < piles.Count * 2 - 1; i++)
+                    for (int i = 0; i < piles.Count * 2; i++)
                     {
                         int matchesToRemove = i % 2 == 0 ? 2 : 1;
 
@@ -138,7 +138,7 @@ namespace MiniMax_Nim_2
                 else
                 {
                     int best = 1;
-                    for (int i = 0; i < piles.Count * 2 - 1; i++)
+                    for (int i = 0; i < piles.Count * 2; i++)
                     {
                         int matchesToRemove = i % 2 == 0 ? 2 : 1;
 
